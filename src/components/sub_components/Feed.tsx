7791a8f9620c6ea49a09c58bbfd2ad25a9d4fdd3
Devpatil8878 <devanandpatil8878gmail.com>
2024-03-25 12:55:08 +0530

    added post feature

--- src/components/sub_components/Feed.tsx
+++ src/components/sub_components/Feed.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 import Link from 'next/link';
@@ -19,50 +19,7 @@ interface FeedProps {
 
 
 function Feed({ isDarkMode }: FeedProps) {
-  let arr: Post[] = [
-    {
-      username: "Saul goodman",
-      image: "https://images.unsplash.com/photo-1682686581498-5e85c7228119?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      post: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, veniam alias vero possimus est voluptates! Eveniet sint est nulla? Ratione ipsum eaque dignissimos, tenetur aliquam quisquam? Tempora sit est nobis.Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, voluptas cumque impedit earum, eveniet voluptatibus soluta tenetur modi autem, mollitia repellat officia velit placeat. Vel culpa quos inventore magnam nam accusantium officia? Modi ex labore ut ducimus sequi rerum voluptas eos eligendi debitis nihil, nam sit a exercitationem? Sequi natus unde, officiis eligendi non libero aut, eum ipsa illum possimus ex pariatur! Delectus quibusdam reprehenderit quos consectetur aspernatur non molestias quisquam quo pariatur. Molestiae eos praesentium, nulla repudiandae exercitationem id? Dolorem cum libero quisquam architecto nesciunt commodi consequuntur est.",
-      date: "date"
-    },
-    {
-      username: "Walter white",
-      image: "https://images.unsplash.com/photo-1679065949530-7bb1fba3ccb3?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      post: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, veniam alias vero possimus est voluptates! Eveniet sint est nulla? Ratione ipsum eaque dignissimos, tenetur aliquam quisquam? Tempora sit est nobis.",
-      date: "date"
-    },
-    {
-      username: "Jason patrick",
-      image: "https://images.unsplash.com/photo-1682685797703-2bb22dbb885b?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      post: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, veniam alias vero possimus est voluptates! Eveniet sint est nulla? Ratione ipsum eaque dignissimos, tenetur aliquam quisquam? Tempora sit est nobis.",
-      date: "date"
-    },
-    {
-      username: "Walter white",
-      image: "https://images.unsplash.com/photo-1679065949530-7bb1fba3ccb3?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      post: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, veniam alias vero possimus est voluptates! Eveniet sint est nulla? Ratione ipsum eaque dignissimos, tenetur aliquam quisquam? Tempora sit est nobis.",
-      date: "date"
-    },
-    {
-      username: "Jason patrick",
-      image: "https://images.unsplash.com/photo-1682685797703-2bb22dbb885b?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      post: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, veniam alias vero possimus est voluptates! Eveniet sint est nulla? Ratione ipsum eaque dignissimos, tenetur aliquam quisquam? Tempora sit est nobis.",
-      date: "date"
-    },
-    {
-      username: "Walter white",
-      image: "https://images.unsplash.com/photo-1679065949530-7bb1fba3ccb3?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      post: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, veniam alias vero possimus est voluptates! Eveniet sint est nulla? Ratione ipsum eaque dignissimos, tenetur aliquam quisquam? Tempora sit est nobis.",
-      date: "date"
-    },
-    {
-      username: "Jason patrick",
-      image: "https://images.unsplash.com/photo-1682685797703-2bb22dbb885b?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      post: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, veniam alias vero possimus est voluptates! Eveniet sint est nulla? Ratione ipsum eaque dignissimos, tenetur aliquam quisquam? Tempora sit est nobis.",
-      date: "date"
-    },
-  ];
+  
 
   useGSAP(()=>{
     gsap.from(".feed-animation",{
@@ -74,22 +31,42 @@ function Feed({ isDarkMode }: FeedProps) {
 })
 
 
+const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchFeed = async () => {
+      try {
+        const response = await fetch('/api/feed');
+        if (!response.ok) {
+          throw new Error('Failed to fetch feed');
+        }
+        const data = await response.json();
+        setPosts(data);
+      } catch (error) {
+        console.error('Error fetching feed:', error);
+      }
+    };
+
+    fetchFeed();
+  }, []);
+
+
 
   return (
     <>
-      {arr.map((elem, index) => (
+      {posts.map((elem, index) => (
         <div key={index} className={`${isDarkMode ? "dark-mode-component-bg" : "light-mode-component-bg"} feed-animation w-[90%] flex-col h-auto mx-10 mt-10 rounded-xl overflow-y-auto`}>
           <div className="flex account-info justify-between">
             <div className="flex mt-3">
-              <Link href={`/user/${elem.username}`}>
+              <Link href={`/user/${elem.user.username}`}>
                 <div className='w-12 h-12 flex justify-center items-center rounded-full m-5 ml-8 mr-3 min-w-12'>
-                  <img className='object-cover w-full h-full rounded-full ' src={`${elem.image}`} alt={elem.username} />
+                  <img className='object-cover w-full h-full rounded-full ' src={`${elem.user.profilePicture}`} alt={elem.user.username} />
                 </div>
               </Link>
-              <Link href={`/user/${elem.username}`}>
+              <Link href={`/user/${elem.user.username}`}>
                 <div className="name flex-col mt-6 ml-6">
-                  <h1 className='text-md font-semibold'>{`${elem.username}`}</h1>
-                  <h4 className='text-xs'>{`${elem.date}`}</h4>
+                  <h1 className='text-md font-semibold'>{`${elem.user.fullname}`}</h1>
+                  <h4 className='text-xs'>{`${elem.createdAt}`}</h4>
                 </div>
               </Link>
             </div>
@@ -102,7 +79,7 @@ function Feed({ isDarkMode }: FeedProps) {
             </Link>
           </div>
           <div className='ml-[3rem] mb-10 pr-20 '>
-            {elem.post}
+            {elem.content}
           </div>
         </div>
       ))}
---
+++ src/pages/api/feed/index.ts
@@ -0,0 +1,28 @@
+import dbConnect from '../../../../utils/dbConnect';
+import Post from '../../../../models/Post'; 
+
+// Connect to the database
+dbConnect();
+
+export default async function handler(req, res) {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ message: 'Method Not Allowed' });
+  }
+
+  try {
+    // Fetch 10 random documents from the 'posts' collection
+    // const randomPosts = await Post.aggregate([{ $sample: { size: 10 } }]).populate('user');
+
+    const randomPosts = await Post.aggregate([
+        { $sample: { size: 10 } },
+        { $lookup: { from: 'users', localField: 'user', foreignField: '_id', as: 'user' } },
+        { $unwind: '$user' } // If user field is an array, unwind it to get individual documents
+      ]);
+
+
+    res.status(200).json(randomPosts);
+  } catch (error) {
+    console.error('Error fetching random posts:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+}
