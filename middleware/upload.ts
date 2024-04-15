2fa43e41f27807655e71f4c8a7f0dfb1d1807494
Devpatil8878 <devanandpatil8878gmail.com>
2024-04-16 05:12:41 +0530

    lkn

---
+++ middleware/upload.ts
@@ -0,0 +1,14 @@
+// middleware/upload.js
+import multer from 'multer';
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: './public/uploads',
+    filename: (req, file, cb) => {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+      cb(null, file.fieldname + '-' + uniqueSuffix);
+    },
+  }),
+});
+
+export default upload;
--- src/app/account/edit-info/page.tsx
+++ src/app/account/edit-info/page.tsx
@@ -1,14 +1,47 @@
 "use client"
 
-import React, { ChangeEvent, FormEvent, useState } from 'react'
+import React, { ChangeEvent, FormEvent, useRef, useState } from 'react'
 import User from '../../../../models/User'; 
 import { useRouter } from 'next/navigation';
+import { useSelector } from 'react-redux';
 
 
 
 function UserDetails() {
 
+  const USERINFO = useSelector(state => state.rootReducer.fullUserInfo)
+
   const router = useRouter()
+  const fileInputRef = useRef(null);
+
+  const [file, setFile] = useState(null);
+  const [status, setStatus] = useState('');
+
+  const handleButtonClick = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleFileChange = async (event) => {
+    const file = event.target.files[0];
+    setFile(event.target.files[0])
+    if (file) {
+      const formData = new FormData();
+      formData.append('file', file);
+
+      try {
+        const response = await fetch('/api/uploadimage', {
+          method: 'POST',
+          body: formData,
+        });
+        const data = await response.json();
+        console.log(data); // Handle response from the server
+      } catch (error) {
+        console.error('Error uploading file:', error);
+        // Handle error
+      }
+    }
+
+  };
 
   const [formData, setFormData] = useState({
     fullname: "",
@@ -45,6 +78,31 @@ function UserDetails() {
     }
   };
 
+  const uploadFile = async () => {
+    if (!file) {
+      setStatus('Please select a file');
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    try {
+      const response = await fetch('/api/uploadimage', {
+        method: 'POST',
+        body: formData,
+      });
+      const data = await response.json();
+
+
+
+      setStatus(data.message);
+    } catch (error) {
+      console.error('Error uploading file:', error);
+      setStatus('An error occurred while uploading the file');
+    }
+  };
+
 
 
 
@@ -52,7 +110,10 @@ function UserDetails() {
     <div className='content-center h-screen bg-black'>
     <div className='w-[75vw] h-[80vh] m-auto content-center bg-black text-center border rounded-xl'>
         <form action="" className='flex-col gap-10 '>
-            <input className='text-black border-2 w-20 h-20 rounded-full ' type="file" name="picture" id="picture" placeholder='' /><br /><br />
+            <div className="prof bg-red-600 border-2 w-20 h-20 rounded-full m-auto mb-10">
+              <input onChange={handleFileChange} ref={fileInputRef} className='text-black hidden' type="file" name="picture" id="picture" placeholder='' /><br /><br />
+              <button onClick={handleButtonClick}>Upload</button>
+            </div>
             
             <div className="names justify-center items-center flex gap-5 ">
               <input className='w-[18%] h-9 p-4 pl-6 text-white bg-zinc-900 rounded-[8px] focus:outline-none placeholder-gray-200 mb-[-5px] text-[14px]' type="text" name='name' placeholder='Full Name' disabled />
