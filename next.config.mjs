67d9b0e6aa93583f4491cd6da05ab9feb35e46cc
ramawtar-123 <ramawtarahirwar34@gmail.com>
2024-04-21 10:46:44 +0530

    account images

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.unsplash.com',
+        port: '',
+      },
+    ],
+  },
     async headers() {
       return [
         {
--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -11,6 +11,20 @@ import { getDatabase, ref, set } from 'firebase/database'
 import Navbar from '@/components/sub_components/Navbar';
 import { setTEMPUSER } from '@/store/actions';
 import { useDispatch } from 'react-redux';
+import Image from 'next/image';
+
+
+  const ImageSources = [
+    'https://plus.unsplash.com/premium_photo-1669115120004-23e5dd04f72d?q=80&w=1432&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1429198739803-7db875882052?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://plus.unsplash.com/premium_photo-1666544584874-c38f42e5e3d4?q=80&w=1527&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1564490292125-2e3c78a0ef44?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1506744038136-46273834b3fb?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1604714628334-febaf95facde?q=80&w=1477&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://plus.unsplash.com/premium_photo-1675359772567-c09110eb33be?q=80&w=1579&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1532382172705-841174a528a7?q=80&w=1376&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  ];
+
 
 
 const firebaseConfig = {
@@ -128,27 +142,44 @@ function Account() {
   return (
     <>
       <Navbar />
-      <div className="main flex gap-32 dark-mode-bg w-full h-[100vh] p-32 pl-[30rem]">
-        <div className="profile gsap w-[10rem] h-[10rem] overflow-hidden rounded-full relative cursor-pointer">
-          <img src={userData.profilePicture || "https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png"} className='object-cover object-center' />
-          <input type="file" name="profile" id="" onChange={handleFileChange} className='absolute w-full h-full z-[-5] '/>
+      <div className="main dark-mode-bg w-full">
+       <div className='front_main  w-8/12 pt-10 ml-72'>
+      
+        <div className='back_profile h-72 w-full bg-zinc-900 rounded-xl mb-[-4rem]'>
+              
         </div>
-        <div className="information gsap w-[20rem] h-[8rem] justify-start">
-          <div className='flex gap-8'>
-            <h1 className="text-2xl">{googleLogged ? user.displayName : userData.fullname}</h1>
-            <button>Edit Profile</button>
-          </div>
+        <div className='flex gap-12 '>
+           <div className="profile gsap w-[10rem] h-[10rem] overflow-hidden rounded-full relative cursor-pointer  ">
+              <img src={userData.profilePicture || "https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png"} className='object-cover object-center' />
+              <input type="file" name="profile" id="" onChange={handleFileChange} className='absolute w-full h-full z-[-5] '/>
+              
+            </div>
+            
+        
 
-          <div className='flex gap-7'>
-            <h1>{}</h1> <span className='ml gsap -[-7%]'>posts</span>
+            <div className="information gsap w-[20rem] h-[8rem] mt-20 ">
+            <h1 className="text-3xl font-semibold fo">{googleLogged ? user.displayName : userData.fullname}</h1>
+               
+
+           <div className='flex gap-7 mt-4'>
+            <h1>{0 }</h1> <span className='ml-[-7%] gsap '>posts</span>
             <h1>{0 }</h1> <span className='ml-[-7%] gsap'>followers</span>
             <h1>{0 }</h1> <span className='ml-[-7%] gsap'>followings</span>
+           </div>
+
+             <h1 className='mt-2 font-semibold gsap'>{userData.name }</h1>
+             <h1 className='font-thin gsap'>{userData.bio }</h1>
+           </div>
           </div>
 
-          <h1 className='mt-4 font-semibold gsap'>{userData.name }</h1>
-          <h1 className='font-thin gsap'>{userData.bio }</h1>
+          <hr></hr>
+          <div className='grid-container mt-10 flex min-h-30 gap-5 min-w-[60vw] '>
+              {ImageSources.map((src,index) => (         
+                 <Image  width={200} height={10} key={index} src={src} alt={''} className='overflow-hidden object-cover'/>
+              ))}
+          </div>
         </div>
-      </div>
+       </div>
     </>
   );
 }
