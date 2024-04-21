e0b8646175bbfca1a98d54f462bcdeabc9728529
ramawtar-123 <ramawtarahirwar34@gmail.com>
2024-04-21 10:58:05 +0530

    j

--- next.config.mjs
+++ next.config.mjs
@@ -4,19 +4,12 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-<<<<<<< HEAD
         hostname: '**.unsplash.com',
-=======
         hostname: '**.pngegg.com',
->>>>>>> e6e92dd5243aea305757ec529aa276860607fd41
         port: '',
       },
     ],
   },
-<<<<<<< HEAD
-=======
-
->>>>>>> e6e92dd5243aea305757ec529aa276860607fd41
     async headers() {
       return [
         {
